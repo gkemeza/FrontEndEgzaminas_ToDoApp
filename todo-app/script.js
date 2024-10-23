e16ef3f6a32a029d3b95cf8b3386ad9168566eae
Gaudrius <gaudriusk@gmail.com>
2024-10-23 21:16:30 +0300

    added show tasks on page load.

--- todo-app/script.js
+++ todo-app/script.js
@@ -1,7 +1,51 @@
 const showName = () => {
-  const header = document.querySelector("header");
+  const header = document.querySelector("h2");
   const username = sessionStorage.getItem("Username");
   header.innerHTML += ` ${username}`;
+};
+
+const showTasks = async () => {
+  try {
+    const url = `https://localhost:7171/api/ToDo`;
+    const response = await fetch(url, {
+      method: `GET`,
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const tasks = await response.json();
+    const userId = sessionStorage.getItem("UserId");
+    const tasksDiv = document.querySelector("#toDo-tasks");
+    const taskContainer = document.createElement("div");
+    taskContainer.id = "task-container";
+
+    tasks.forEach((task) => {
+      if (task.userId === `${userId}`) {
+        console.log(task);
+        const date = new Date(task.endDate).toLocaleDateString();
+
+        const taskElement = document.createElement("div");
+        taskElement.className = "task-card";
+        taskElement.innerHTML = `
+        <h3>${task.type}</h3>
+        <p>${task.content}</p>
+        <p>Due: ${date}</p>
+        <button onclick="updateTask(${task.id})">Update</button>
+        <button onclick="deleteTask(${task.id})">Delete</button>
+      `;
+
+        taskContainer.append(taskElement);
+      }
+    });
+
+    tasksDiv.replaceChildren(); // remove existing tasks?
+    tasksDiv.append(taskContainer);
+  } catch (error) {
+    console.error("Detailed error:", error);
+    alert(`Error loading tasks: ${error}`);
+  }
 };
 
 const createTask = async (event) => {
@@ -38,8 +82,6 @@ const createTask = async (event) => {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
 
-    // const content = await response.text();
-    // alert(`Response: `, content);
     form.reset(); // clear input fields
   } catch (error) {
     console.error("Detailed error:", error);
@@ -65,7 +107,12 @@ const openForm = () => {
 
   // Add the form submit handler
   form.addEventListener("submit", createTask);
-  document.body.append(form);
+  document.querySelector("#toDo-options").append(form);
 };
 
-window.onload = showName;
+const initialData = () => {
+  showName();
+  showTasks();
+};
+
+window.onload = initialData;
