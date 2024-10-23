0475117648bdda317d8ed38739caed1b9122fb92
Gaudrius <gaudriusk@gmail.com>
2024-10-23 18:43:56 +0300

    Updated registration to save id and username to session storage.

--- registration-form/script.js
+++ registration-form/script.js
@@ -1,3 +1,25 @@
+const saveIdAndUsername = async (username, password) => {
+  try {
+    const url = `https://localhost:7171/api/Auth?username=${username}&password=${password}`;
+    const response = await fetch(url, {
+      method: `GET`,
+    });
+
+    if (!response.ok) {
+      const errorData = await response.text();
+      console.error("Server error:", errorData);
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const content = await response.json();
+    sessionStorage.setItem("UserId", content.id);
+    sessionStorage.setItem("Username", content.userName);
+  } catch (error) {
+    console.error(`Error: `, error);
+    throw error;
+  }
+};
+
 const onRegister = async () => {
   const username = document.querySelector(`#signin-name`).value.trim();
   const password = document.querySelector(`#signin-password`).value.trim();
@@ -26,9 +48,13 @@ const onRegister = async () => {
       console.error("Server error:", errorData);
       throw new Error(`HTTP error! status: ${response.status}`);
     }
+
+    // use login fetch to get user id
+    await saveIdAndUsername(username, password);
+
+    window.location.href = "../todo-app/todo.html"; // go to TO DO page
   } catch (error) {
     console.error(`Error: `, error);
+    return;
   }
-
-  //   window.location.href = "../registration-form/register.html"; // go to TO DO page
 };
